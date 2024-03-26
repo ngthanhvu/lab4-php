1d533672851996b112ea7dc6acaaa4bb861f5a71
ngthanhvu <nguyenthanhvudvfb@gmail.com>
2024-03-26 08:30:38 +0700

    Update new

--- edit-user.php
+++ edit-user.php
@@ -1,28 +1,37 @@
 <?php
 session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['username']) && !empty($_POST['username'])) {
-        $usernameToUpdate = $_POST['username'];
-        $newPassword = isset($_POST['password']) ? $_POST['password'] : ''; 
-        $newRole = isset($_POST['role']) ? $_POST['role'] : ''; 
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+     if (isset($_POST['username'])) {
+          if (!empty($_POST['username'])) {
+               $usernameupdate = $_POST['username'];
+               if (isset($_POST['password'])) {
+                    $newpassword = $_POST['password'];
+               } else {
+                    $newpassword = "";
+               }
+               if (isset($_POST['role'])) {
+                    $newrole = $_POST['role'];
+               } else {
+                    $newrole = $_POST['role'];
+               }
 
-        if (isset($_SESSION['danhSachUser'])) {
-            foreach ($_SESSION['danhSachUser'] as $khoa => $user) {
-                if ($user['username'] == $usernameToUpdate) {
-                    $user['password'] = $newPassword; 
-                    $user['role'] = $newRole; 
-                    $_SESSION['danhSachUser'][$khoa] = $user;
-                    break; 
-                }
-            }
-            unset($user); 
-        }
-        header("Location: quanly.php");
-        exit();
-    }
+               if ($_SESSION['danhSachUser']) {
+                    foreach ($_SESSION['danhSachUser'] as $key => $user) {
+                         if ($user['username'] == $usernameupdate) {
+                              $user['password'] = $newpassword;
+                              $user['role'] = $newrole;
+                              $_SESSION['danhSachUser'][$key] = $user;
+                              break;
+                         }
+                    }
+                    unset($user);
+               }
+               header("Location: quanly.php");
+               exit();
+          }
+     }
+
 }
-
-
 ?>
 
 
@@ -32,22 +41,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chỉnh Sửa Người Dùng</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h2>Chỉnh Sửa Người Dùng</h2>
-    <form action="edit-user.php" method="POST">
-        <label for="username">Tên Người Dùng:</label><br>
-        <input type="text" id="username" name="username" required><br>
-
-        <label for="password">Mật Khẩu Mới:</label><br>
-        <input type="password" id="password" name="password" required><br>
-
-        <label for="role">Vai Trò:</label><br>
-        <select id="role" name="role">
-            <option value="user">User</option>
-            <option value="admin">Admin</option>
-        </select><br>
-        <input type="submit" value="Cập Nhật">
-    </form>
+    <div class="container">
+        <h2>Chỉnh Sửa Người Dùng</h2>
+        <form action="edit-user.php" method="POST">
+            <label class="form-label" for="username">Tên Người Dùng:</label><br>
+            <input class="form-control" type="text" id="username" name="username" required><br>
+    
+            <label class="form-label" for="password">Mật Khẩu Mới:</label><br>
+            <input placeholder="Enter new password" class="form-control" type="password" id="password" name="password" required><br>
+    
+            <label class="form-label" for="role">Vai Trò:</label><br>
+            <select class="form-select" id="role" name="role">
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+            </select><br>
+            <input class="btn btn-primary" type="submit" value="Cập Nhật">
+        </form>
+    </div>
 </body>
 </html>
